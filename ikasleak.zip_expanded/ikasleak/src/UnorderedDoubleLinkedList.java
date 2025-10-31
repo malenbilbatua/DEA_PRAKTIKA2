d4f66bb369fbe7a3d2dfd473f0845d08630f28a1
uporter001 <uporter001@gmail.com>
2025-10-31 08:59:54 +0100

    Add addAfter method to UnorderedDoubleLinkedList
    Implemented addAfter method to insert an element after a target in the list.

--- ikasleak.zip_expanded/ikasleak/src/UnorderedDoubleLinkedList.java
+++ ikasleak.zip_expanded/ikasleak/src/UnorderedDoubleLinkedList.java
@@ -14,7 +14,7 @@ public void addToRear(T elem) {
 		Node<T> berria = new Node<T>(elem);
         if (this.last == null ) {
 			this.last = berria;
-			count = 1;
+			this.count = 1;
 		} else if (last.next == null) {
 			last.next = berria;
 			berria.prev = last;
@@ -32,11 +32,42 @@ public void addToRear(T elem) {
 			this.count = this.count + 1;
 		}
 	}
-
 	
-	public void addAfter(T elem, T target) {
-		// KODEA OSATU ETA KOSTUA KALKULATU
+		public void addAfter(T elem, T target) {
+	//target elementua eta gero gehitu
+	//KOSTUA: n = zerrendaren aldagai kopurua izanda, O(n); lineala				
+		Node<T> berria = new Node<T>(elem);
+		if (last.next == null && last.data.equals(target)) {
+			this.addToRear(elem);
+		} else {
+			boolean aurkitua = false;
+			Node<T> unekoa = last.next;
+			Node<T> hurrengoa = unekoa.next;
+			while (unekoa != last && !aurkitua) {
+				if (unekoa.data.equals(target)) {
+					aurkitua = true;
+				} else {
+					unekoa = hurrengoa;
+					hurrengoa = hurrengoa.next;
+				}
+			}
+			if (last.data.equals(target)) {
+				Node<T> first = last.next;
+				berria.next = first;
+				last.next = berria;
+				first.prev = berria;
+				berria.prev = last;
+				last = berria;
+				this.count = this.count + 1;
+			} else if (aurkitua && unekoa != last) {
+				berria.next = hurrengoa;
+				unekoa.next = berria;
+				hurrengoa.prev = berria;
+				berria.prev = unekoa;
+				this.count = this.count + 1;
+			}
+		}
 	}
-
 }
+
 
